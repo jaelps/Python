7c87fb099d0ecdc372e79ee5c23e2d9b59085389
jaelps <jaelsonpsilva18@gmail.com>
2022-11-02 21:50:45 -0300

    primeiros exercicios

---
+++ main.py
@@ -0,0 +1,22 @@
+#print('Ola mundo!')
+
+#nome ='Guanabara'
+#idade = 25
+#peso = 75.5
+
+#print(nome, idade, peso)
+
+#-------------------------------------------
+
+#nome = input('Qual seu nome?  ')
+#idade = input('Qual sua idade?  ')
+#peso = input('Qual seu peso?  ')
+
+#print(nome, idade, peso)
+
+#--------------------------------------------
+#desafio de python cuso em video 
+nome = input('Qual seu nome? ')
+
+print(f'Bem vindo o {nome} ao programa de aprendizado python !!')
+
